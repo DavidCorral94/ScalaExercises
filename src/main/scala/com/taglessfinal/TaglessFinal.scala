093ef0540762ba4b76aa49d944f752de17da77ba
David Corral Plaza <david.corral@47deg.com>
2021-06-04 14:02:23 +0200

    Improving TF example WIP

--- src/main/scala/com/taglessfinal/TaglessFinal.scala
+++ src/main/scala/com/taglessfinal/TaglessFinal.scala
@@ -2,7 +2,7 @@ package com.taglessfinal
 
 import cats.Monad
 import cats.effect.{ExitCode, IO, IOApp, Sync}
-import com.taglessfinal.DataTypes.User
+import com.taglessfinal.DataTypes.{Email, User}
 import com.taglessfinal.TypeClasses._
 
 object DataTypes {
@@ -13,17 +13,30 @@ object DataTypes {
 object TypeClasses {
 
   trait UserRepository[F[_]] {
+    def persistUser(user: User): F[Boolean]
     def findUser(id: Int): F[Option[User]]
+    def updateUser(id: Int): F[Boolean]
   }
 
   object UserRepository {
+
+    // Simulating a DB
+    var db: Map[Int, User] = Map().empty
 
     def apply[F[_]](implicit F: UserRepository[F]): UserRepository[F] = F
 
     implicit def instance[F[_]: Monad]: UserRepository[F] =
       new UserRepository[F] {
-        override def findUser(id: Int): F[Option[User]] =
+
+        def persistUser(user: User): F[Boolean] = {
+          db = db + (user.id -> user)
+          Monad[F].pure(true)
+        }
+
+        def findUser(id: Int): F[Option[User]] =
           Monad[F].pure(Option(User(id, "David", List())))
+
+        def addEmail(id: Int, email: Email ): F[Boolean] = ???
       }
   }
 }
@@ -40,7 +53,7 @@ object Main extends App {
   val userList = UserRepository[List].findUser(1)
   userList.foreach {
     case Some(u) => println(u)
-    case None => println("User not found")
+    case None    => println("User not found")
   }
 
 }
